fb28975226ac9c0a45552c0f5ea91ec72968ef22
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-06-09 17:17:19 +0700

    chore: basic structure

--- app/src/main/java/com/example/cft_loan/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/cft_loan/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cft_loan
+package com.example.cft_loan.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cft_loan.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
