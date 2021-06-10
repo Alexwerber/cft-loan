52dd301eb24bde2c8ac79be2d50dcb4f634b154c
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-06-10 17:06:19 +0700

    feat: add POST and GET methods in ApiService and UserInfo entity

--- app/src/main/java/com/example/cft_loan/data/remote/ApiService.kt
+++ app/src/main/java/com/example/cft_loan/data/remote/ApiService.kt
@@ -1,8 +1,20 @@
 package com.example.cft_loan.data.remote
 
+import com.example.cft_loan.data.entities.Loan
+import com.example.cft_loan.data.entities.UserInfo
+import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("/loans/all")
-    fun getLoansList()
+    @GET("loans/all")
+    fun getLoansList(@Query("") token: String): Call<Loan>
+
+    @POST("registration")
+    fun createUser(@Body userInfo: UserInfo): Call<UserInfo>
+
+    @POST("login")
+    fun loginUser(@Body userInfo: UserInfo): Call<String>
 }
