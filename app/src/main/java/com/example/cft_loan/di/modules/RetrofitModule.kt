f085dff5a19eb3c7555dc12e0a16a5e5569c19a2
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-06-10 17:16:11 +0700

    feat: add Retrofit module and api constants

--- app/src/main/java/com/example/cft_loan/di/modules/RetrofitModule.kt
+++ app/src/main/java/com/example/cft_loan/di/modules/RetrofitModule.kt
@@ -1,7 +1,35 @@
 package com.example.cft_loan.di.modules
 
+import com.example.cft_loan.data.remote.ApiService
+import com.example.cft_loan.di.constants.Api.BASE_URL
+import com.example.cft_loan.di.constants.Api.READ_TIMEOUT
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 class RetrofitModule {
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient = OkHttpClient.Builder().run {
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        addInterceptor(interceptor)
+        readTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS)
+        build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(okHttpClient: OkHttpClient): ApiService =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(ApiService::class.java)
 }
