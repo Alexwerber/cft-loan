7acfa07b564735c090e80473b00e72844ebdd847
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-06-15 15:34:02 +0700

    feat: add chech input

--- app/src/main/java/com/example/cft_loan/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/cft_loan/ui/fragments/LoginFragment.kt
@@ -2,7 +2,22 @@ package com.example.cft_loan.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.example.cft_loan.R
+import com.example.cft_loan.errorcheck.CheckLoginAndPassword
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
+    private val checkInput = CheckLoginAndPassword()
 
+    override fun onStart() {
+        super.onStart()
+
+        confirm_button.setOnClickListener {
+            val login = login_name.text.toString()
+            val password = login_password.text.toString()
+
+            if(checkInput.checkLogin(login) && checkInput.checkPassword(password)) {
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/cft_loan/ui/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/example/cft_loan/ui/fragments/RegistrationFragment.kt
@@ -3,9 +3,12 @@ package com.example.cft_loan.ui.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.cft_loan.R
+import com.example.cft_loan.errorcheck.CheckLoginAndPassword
 import kotlinx.android.synthetic.main.fragment_registration.*
+import kotlinx.android.synthetic.main.fragment_registration.confirm_button
 
 class RegistrationFragment: Fragment(R.layout.fragment_registration) {
+    private val checkInput = CheckLoginAndPassword()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +17,14 @@ class RegistrationFragment: Fragment(R.layout.fragment_registration) {
     override fun onStart() {
         super.onStart()
 
+        confirm_button.setOnClickListener {
+            val login = registration_name.text.toString()
+            val password = registration_password.text.toString()
 
+            if(checkInput.checkLogin(login) && checkInput.checkPassword(password)) {
+
+            }
+        }
 
         if_user_already_registered.setOnClickListener {
             activity?.let {
